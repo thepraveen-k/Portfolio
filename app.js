1a5c3b3ca0c424640a3a5e5fd7debdee59c38aec
Praveen K <127839101+thepraveen-k@users.noreply.github.com>
2024-05-11 18:46:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     const typed = new Typed('.multiple-text', {
-        strings: ['Back-end Developer', 'Software Developer'],
+        strings: ['Software Developer'],
         typeSpeed: 70,
         backSpeed: 70,
         backDelay: 1000,
