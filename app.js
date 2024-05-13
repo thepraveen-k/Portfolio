25134fbf63cf3919ff5581b956bee106456c81a4
Praveen K <127839101+thepraveen-k@users.noreply.github.com>
2024-05-13 17:38:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ScrollReveal({
         distance: "80px",
         duration: 2000,
-        delay: 200,  //
+        delay: 200,  
     });
 
     ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
